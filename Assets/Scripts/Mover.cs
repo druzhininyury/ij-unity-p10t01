48dc0cf8040fe579196f69eb7239af41fc59f577
Druzhinin Yury <druzhinin.yy@gmail.com>
2026-01-08 17:20:01 +0300

    Simplify logic.

--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -3,27 +3,9 @@ using UnityEngine;
 public class Mover : MonoBehaviour
 {
     [SerializeField] private float _speed;
-    [SerializeField] private float _distance;
     
-    private Vector3 _startPosition;
-    private float _currentDistance;
-    
-    // Start is called before the first frame update
-    void Start()
-    {
-        _startPosition = transform.position;
-        _currentDistance = 0f;
-    }
-
-    // Update is called once per frame
     void Update()
     {
-        _currentDistance = (_currentDistance + _speed * Time.deltaTime) % (2 * _distance);
-        float shift = _currentDistance <= _distance ? _currentDistance : 2 * _distance - _currentDistance;
-        
-        Vector3 nextPosition = _startPosition;
-        nextPosition.x += shift;
-        
-        transform.position = nextPosition;
+        transform.position += transform.forward * (_speed * Time.deltaTime);
     }
 }
